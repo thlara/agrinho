da625170236c440107bb60e0a56d11adba8202ae
thlara <105953614+thlara@users.noreply.github.com>
2024-11-21 20:43:57 -0300

    rr

--- src/js/main.js
+++ src/js/main.js
@@ -13,5 +13,14 @@ const navItens = $('.nav_item');
 
 $(window).on('scroll', function () {
     const header = $('header');
-    
+    const scrollPosition = $(window).scrollTop() -
+    header.outerHeigth();
+
+    let activeSectionIndex = 0;
+     if (scrollPosition <= 0) {
+        header.css('box-shadow',  'none');
+     } else {
+        header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1')
+        
+     }
 })
